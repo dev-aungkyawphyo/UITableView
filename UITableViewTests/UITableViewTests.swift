c0680b9ddbb532a5e46907611fc39c67d7042d20
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-12-05 00:25:08 +0630

    Initial Commit

---
+++ UITableViewTests/UITableViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  UITableViewTests.swift
+//  UITableViewTests
+//
+//  Created by Aung Kyaw Phyo on 05/12/2024.
+//
+
+import Testing
+@testable import UITableView
+
+struct UITableViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
